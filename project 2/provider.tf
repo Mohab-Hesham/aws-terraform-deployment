2a26b925824753fb4d876f138ad2a6f15a542d91
Mohab Hesham <161193942+Mohab-Hesham@users.noreply.github.com>
2024-03-12 16:39:59 +0200

    Add files via upload

--- project 2/provider.tf
+++ project 2/provider.tf
@@ -3,11 +3,11 @@ terraform {
   backend "s3" {
   bucket = "bucket---mo"
   key    = "state"
-  region = "us-east-1"
+  region = "us-east-2"
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
